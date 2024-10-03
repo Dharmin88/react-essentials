35462ef589f3cda316154c26ed16995c8ee0ed62
Dharmin Shah <shahdharmin8127@gmail.com>
2024-10-03 18:42:14 +0530

    Using props in the Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import coreConcepts from './assets/react-core-concepts.png';
+
+import componentsImg from './assets/components.png';
+import propsImg from './assets/config.png';
+
 
 const reactDescription = ['Fundamental', 'Crucial', 'Core'];
 
@@ -12,23 +17,24 @@ function Header() {
   const description = reactDescription[getRandomInt(2)];
   
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+    <header>
+      <img src={coreConcepts} alt="Stylized atom" />
+      <h1>React Essentials</h1>
       <p>
-        Edit <code>src/App.js</code> and save to reload.
+        {description} React concepts you will need for almost any app you are
+        going to build!
       </p>
-      <p>
-        {description} React concepts you will need for almost any app you are going to build!
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
     </header>
+  );
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
   );
 }
 
@@ -36,6 +42,25 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title="Components" 
+              description="The core YI building block."
+              image={componentsImg}
+            />
+            <CoreConcept 
+              title="Props"
+              description=""
+              image={propsImg}
+            />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
