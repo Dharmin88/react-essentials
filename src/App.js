0b246503d76c3bed209e90f125fd3d7cb2c187fc
Dharmin Shah <shahdharmin8127@gmail.com>
2024-10-04 13:08:15 +0530

    Create Separate files for components and component related CSS and moved to separate directories.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
 // import logo from './logo.svg';
+import Header from './components/Header/Header';
+import CoreConcept from './components/CoreConcepts/CoreConcepts';
 import './App.css';
-import coreConcepts from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data';
-
-
-const reactDescription = ['Fundamental', 'Crucial', 'Core'];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-
-  const description = reactDescription[getRandomInt(2)];
-  
-  return (
-    <header>
-      <img src={coreConcepts} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept( {image, title, description} ) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
